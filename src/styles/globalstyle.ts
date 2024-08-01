1f1a99fc44f946592ea1e8aa4aadd78e92efc3db
Gabrisaa <gabrielavilelaneto35@gmail.com>
2024-08-01 10:40:27 -0300

    first commit

--- src/styles/globalstyle.ts
+++ src/styles/globalstyle.ts
@@ -2,9 +2,10 @@ import { PaytoneOne_400Regular } from '@expo-google-fonts/paytone-one'
 import {StyleSheet} from 'react-native'
 
 export const colors = {
-    gray: '#b8b3b3',
+    gray: '#ffcdd4',
     light: '#e6e5e5',
-    primary: "#859ebf",
-    secondary: "#fbb894",
-    third: "#fbd8b9",
+    primary: "#e57d90",
+    secondary: "#d0c2cf",
+    third: "#fe5668",
+    black: "#000000",
 }
